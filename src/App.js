ba85f93c2218d0837414f043f0832ccf3862ce93
Zack DB DEV <zackinfosbd@gmail.com>
2021-07-14 16:25:55 +0100

    setting up the envirnoment variables

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_ACESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
@@ -11,7 +12,7 @@ function App() {
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}?client_id=k7QMEpMsd0aXqQ18s4ZFSa7sSE6P-cOvQaRtyvxYBTI`;
+    url = `${mainUrl}${clientID}`;
     try {
       const response = await fetch(url);
       const data = await response.json();
